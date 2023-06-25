ca9e1e9d75d4fc38f3ea24361e8196749ed56b20
Olga <o.panova27@gmail.com>
2023-06-25 09:03:42 +0300

    Rename variables

--- main.py
+++ main.py
@@ -22,18 +22,18 @@ class TelegramLogsHandler(logging.Handler):
 
 def main():
     load_dotenv()
-    TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
-    DEVMAN_TOKEN = os.getenv("DEVMAN_TOKEN")
-    TG_CHAT_ID = os.getenv("TG_CHAT_ID")
+    tg_bot_token = os.getenv("TG_BOT_TOKEN")
+    devman_token = os.getenv("DEVMAN_TOKEN")
+    tg_chat_id = os.getenv("TG_CHAT_ID")
 
     logger.setLevel(logging.DEBUG)
-    logger.addHandler(TelegramLogsHandler(TG_BOT_TOKEN, TG_CHAT_ID))
-    bot = telegram.Bot(token=TG_BOT_TOKEN)
+    logger.addHandler(TelegramLogsHandler(tg_bot_token, tg_chat_id))
+    bot = telegram.Bot(token=tg_bot_token)
     logger.info('Бот запущен')
     url = 'https://dvmn.org/api/long_polling/'
     payload = {}
     headers = {
-        'Authorization': f'Token {DEVMAN_TOKEN}'
+        'Authorization': f'Token {devman_token}'
     }
 
     while True:
@@ -56,7 +56,7 @@ def main():
                     lesson_url = attempt['lesson_url']
                     if is_negative:
                         bot.send_message(
-                            chat_id=TG_CHAT_ID,
+                            chat_id=tg_chat_id,
                             text=f'''У вас проверили работу «{lesson_title}».
 
                             К сожалению в работе нашлись ошибки.
@@ -65,7 +65,7 @@ def main():
                         )
                     else:
                         bot.send_message(
-                            chat_id=TG_CHAT_ID,
+                            chat_id=tg_chat_id,
                             text=f'''У вас проверили работу «{lesson_title}».
 
                             Преподавателю всё понравилось, можно приступать к следующему уроку!
